4c1ef745e10630aba5d8cf956a4b22dce894d6a6
MrDuch <shatohinal@gmail.com>
2025-05-11 00:07:14 +0300

    Add several get methods

--- app/students/router.py
+++ app/students/router.py
@@ -1,12 +1,28 @@
 from typing import List
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from app.students.dao import StudentDAO
+from app.students.rb import RBStudent
 from app.students.schemas import SStudent
 
 
 router = APIRouter(prefix='/students', tags=['Работа со студентами'])
 
 
-@router.get("/", summary="Получить всех студентов", response_model=List[SStudent])
-async def get_all_students():
-    return await StudentDAO.find_all()
+@router.get("/", summary="Получить всех студентов")
+async def get_all_students(request_body: RBStudent = Depends()) -> list[SStudent]:
+    return await StudentDAO.find_all(**request_body.to_dict())
+
+@router.get("/{id}", summary="Получить одного студента по id")
+async def get_student_by_id(student_id: int) -> SStudent | dict:
+    result = await StudentDAO.find_one_or_none_by_id(student_id)
+    if result is None:
+        return {'message': f'Студент с ID {student_id} не найден!'}
+    return result
+
+@router.get("/by_filter", summary="Получить одного студента по фильтру")
+async def get_student_by_filter(request_body: RBStudent = Depends()) -> SStudent | dict:
+    rez = await StudentDAO.find_one_or_none(**request_body.to_dict())
+    if rez is None:
+        return {'message': f'Студент с указанными вами параметрами не найден!'}
+    return rez
+  
