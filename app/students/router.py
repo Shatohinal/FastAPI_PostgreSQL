1a4bf440bc701880420805ff54a6b189871b4a01
MrDuch <shatohinal@gmail.com>
2025-05-10 23:33:21 +0300

    Add response_model, figured out how to add List

--- app/students/router.py
+++ app/students/router.py
@@ -1,10 +1,12 @@
+from typing import List
 from fastapi import APIRouter
 from app.students.dao import StudentDAO
+from app.students.schemas import SStudent
 
 
 router = APIRouter(prefix='/students', tags=['Работа со студентами'])
 
 
-@router.get("/", summary="Получить всех студентов")
+@router.get("/", summary="Получить всех студентов", response_model=List[SStudent])
 async def get_all_students():
     return await StudentDAO.find_all_students()
